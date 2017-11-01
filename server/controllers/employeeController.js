9366bf0fcc2d9e23f170c393fdf5abc751c2be46
Sharath Bhat <Sharath Bhat>
2017-11-02 00:49:32 +0530

    front end!!!

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -7,9 +7,22 @@ var connection = mysql.createConnection({
   database : 'realestate'
 });
 
-//
+//SEND the data to the employee to verify the documents
 module.exports.getverify = function(req,res) {
   connection.query('SELECT * FROM document WHERE documentid in (SELECT verification.documentid WHERE verification.employeeid = ?)',[req.body.employeeid],function(err,result){
+    if(err) {
+      console.log(err);
+      res.send({success: false});
+    }
+    else {
+      res.send([{success: true,data:result}]);
+    }
+  });
+}
+
+
+module.exports.verify = function(req,res) {
+  connection.query('UPDATE verification SET status = 1WHERE employeeid = ? AND documentid = ?',[req.body.employeeid,req.body.documentid],function(err,result){
     if(err) {
       console.log(err);
       res.send({success: false});
