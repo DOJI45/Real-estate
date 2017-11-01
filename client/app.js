dfa2cd43bc039ca2f20d60fd9ea37d315f726d48
Sharath Bhat <Sharath Bhat>
2017-11-01 13:13:55 +0530

    controllers added

---
+++ client/app.js
@@ -0,0 +1,45 @@
+var app = angular.module("realApp", ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/login', {
+    templateUrl: '/views/login.html',
+    controller: 'loginController'
+  })
+  .when('/signup', {
+    templateUrl: '/views/signup.html',
+    controller: 'loginController'
+  })
+  .when('/home', {
+    templateUrl: '/views/home.html',
+    controller: 'homeController'
+  })
+  .when('/employee', {
+    templateUrl: '/views/employee.html',
+    controller: 'employeeController'
+  })
+  .when('/property', {
+    templateUrl: '/views/property.html',
+    controller: 'homeController'
+  })
+  .when('/upload', {
+    templateUrl: '/views/upload.html',
+    controller: 'homeController'
+  })
+  .when('/notification', {
+    templateUrl: '/views/notification.html',
+    controller: 'homeController'
+  })
+  .when('/wishlist', {
+    templateUrl: '/views/wishlist.html',
+    controller: 'homeController'
+  })
+  .otherwise({
+    redirectTo: '/login'
+  })
+})
+
+
+app.controller('mainController', function($scope) {
+  $scope.main = "Real Estate Management";
+})
